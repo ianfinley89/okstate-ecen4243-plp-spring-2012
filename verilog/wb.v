2bc1e6dd50dab41afa815823a563c4c53a04afbf
jadick1988 <jadick1988@gmail.com>
2012-04-06 18:46:43 +0000

    changed lines 8,13,28-30,36

--- verilog/wb.v
+++ verilog/wb.v
@@ -5,12 +5,12 @@ module wb(
  //data
  input        [`W_DATA-1:0]     data_word,
  input        [`W_DATA-1:0]     jalra,
- input        [`W_DATA-1:0]        alu_r,
+ input        [`W_DATA-1:0]     alu_r,
  //control
  input        [1:0]             lbu_byte,
  input        [1:0]             c_wb_src,
  //output
- output        [`W_DATA-1:0]     wb_data
+ output        [`W_DATA-1:0]    wb_data
 );
 
 `include "constant_params.vh"
@@ -25,15 +25,15 @@ wire [`W_DATA-1:0] data_byte_ze;
 // MUX_DATA_BYTE_1
 assign data_byte =
   (lbu_byte== 2'd0) ? data_word[7:0]:
-  (lbu_byte== 2’d1) ? data_word[15:8]:
-  (lbu_byte== 2’d2) ? data_word[23:16]:
-  (lbu_byte== 2’d3) ? data_word[31:24]:
+  (lbu_byte== 2'Â’d1) ? data_word[15:8]:
+  (lbu_byte== 2Â’'d2) ? data_word[23:16]:
+  (lbu_byte== 2'Â’d3) ? data_word[31:24]:
                       data_word[7:0];
   
 
 // MUX_DATA_BYTE_
 
-assign data_byte_ze = {24’b0, data_byte}; // NOTE: no dependence on W_DATA
+assign data_byte_ze = [24Â’b0, data_byte]; // NOTE: no dependence on W_DATA
 assign wb_data =
  (c_wb_src== MUX_DBYTE) ? data_byte_ze:
  (c_wb_src== MUX_JALRA) ? jalra:
