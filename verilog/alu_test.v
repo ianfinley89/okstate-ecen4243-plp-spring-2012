7584d0def38299f38391f083e0f1eb6a26f0abcb
gaalswy <gaalswy@ECE-101F-2.ad.okstate.edu>
2012-04-20 13:43:30 -0500

    - complete alu testbench

--- verilog/alu_test.v
+++ verilog/alu_test.v
@@ -64,7 +64,7 @@ module alu_test;
 		status("init");
         
 		// Add stimulus here
-		rfa = 32'h5;
+		rfa = 32'hfffffffc;
 		rfb = 32'h4;
 		c_aluy_src = MUX_RFB;
 		alu_func = F_ADD;
@@ -72,21 +72,81 @@ module alu_test;
 		
 		
 		rfa = 32'h6;
-		status("");
+		status("add");
+
 		
 		alu_func = F_XOR;
 		status("xor");
 		alu_func = F_MULLO;
 		status("mullo");
+
+		rfa = 32'h0fffffff;
+		rfb = 32'h3000;
 		alu_func = F_MULHI;
-		status("mulhi");
+		status("mulhi - 0x2FF expected");
+
+		alu_shamt = 32'h4;
+		rfb = 32'hf0;
+    alu_func = F_LSHIFT;
+    status("left shift");
+
+    alu_func = F_RSHIFT;
+    status("right shift");
+
+    rfa = 32'h6;
+    rfb = 32'h4;
+    alu_func = F_SUB;
+    status("subtraction");
+
+    rfb = 32'h7;
+    status("subtraction");
+
+    rfb = 32'hf0f0f0f0;
+    rfa = 32'h0f0f0f0f;
+    alu_func = F_OR;
+    status("or");
+
+    alu_func = F_NOR;
+    status("nor");
+
+    rfa = 32'h5;
+    rfb = 32'hfffffffc;
+    alu_func = F_CMP_S;
+    status("cmp signed");
+
+    alu_func = F_CMP_U;
+    status("cmp unsigned");
+
+    rfb = 32'hfffffffb;
+    status("cmp signed");
+
+    alu_func = F_CMP_U;
+    status("cmp unsigned");
+
+    alu_func = F_NEQ;
+    status("not equal");
+    alu_func = F_EQ;
+    status("equals");
 		
+    rfa = 32'h7;
+    rfb = 32'h7;
+    alu_func = F_NEQ;
+    status("not equal");
+    alu_func = F_EQ;
+    status("equals");
+    
+    rfa = 32'h0;
+    imm32 = 32'h10;
+    c_aluy_src = MUX_IMM;
+    alu_func = F_OR;
+    status("src is im32, func OR");
+
 		$stop;
 	end
       
 	// always @(rfa or rfb or c_aluy_src or imm32 or alu_func or alu_shamt) begin
 	task status;
-	input [80:1] str;
+	input [640:1] str;
 	begin
 		#1;
 		$display("== %s ==", str);
